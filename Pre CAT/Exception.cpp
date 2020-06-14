0b024df82b4d138ad632f2434606246a43dc91c2
nipun-madaan <nipun_madaan@yahoo.com>
2020-06-14 19:06:25 +0530

    Added Exception

---
+++ Pre CAT/Exception.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=0;
+    cout<<"Before divide"<<endl;
+    int c=a/b;  //When some part of code makes the program terminate abnormally then this is called as exception
+    cout<<"After divide"<<endl;
+    cout<<"Exception Occured"<<endl;
+}
