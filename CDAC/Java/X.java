857fba8b755f4de31779740ead95269c9b13ed44
nipun-madaan <nipun_madaan@yahoo.com>
2020-06-23 21:17:00 +0530

    Constructor chaining

---
+++ CDAC/Java/X.java
@@ -0,0 +1,19 @@
+class X{
+	private int a, b;
+	
+	public X(){
+		this(10,20);
+		System.out.println("X()");
+	}
+	
+	public X(int a,int b){
+		this();
+		//super();
+		this.a=a;
+		this.b=b;
+		System.out.println("X(int,int)");
+	}
+	public static void main(String [] args){
+		X x= new X(10,20);
+	}
+}
