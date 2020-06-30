5d5967952ca157a57a7c657b6c5e7c6edb59545d
nipun-madaan <nipun_madaan@yahoo.com>
2020-06-30 21:26:51 +0530

    Inheritance

---
+++ CDAC/Java_Team_6/Base.java
@@ -0,0 +1,5 @@
+class Base{
+	public static void main(String[] args){
+		System.out.println("Base");
+	}
+}
---
+++ CDAC/Java_Team_6/Derived.java
@@ -0,0 +1,5 @@
+class Derived extends Base{
+	public static void main(String[] args){
+		System.out.println("Derived");
+	}
+}
