e9994a1ee60764a6153955e55f1a40e1312d43ab
nipun-madaan <nipun_madaan@yahoo.com>
2020-07-02 22:03:07 +0530

    function overloading

---
+++ CDAC/Java_Team_6/B1.java
@@ -0,0 +1,9 @@
+class B1{
+	public void test(int x){
+		System.out.println("B1::test(int)");
+	}
+	
+	public void test(int x,int y){
+		System.out.println("B1::test(int,int)");
+	}
+}
---
+++ CDAC/Java_Team_6/B2.java
@@ -0,0 +1,12 @@
+class B2 extends B1{
+	private void test(int x,int y,int z){
+		System.out.println("B2::test(int)");
+	}
+	
+	public static void main(String[] args){
+		B1 b1 = new B1();
+		b1.test(10);
+		b1.test(10,20);
+		//b1.test(10,20,30);
+	}
+}
