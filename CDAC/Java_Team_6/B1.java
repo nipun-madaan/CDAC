c6d878b72684b7aad0f3c44259b04645dd5e1c09
nipun-madaan <nipun_madaan@yahoo.com>
2020-07-02 22:05:09 +0530

    function overloading

--- CDAC/Java_Team_6/B1.java
+++ CDAC/Java_Team_6/B1.java
@@ -2,8 +2,13 @@ class B1{
 	public void test(int x){
 		System.out.println("B1::test(int)");
 	}
-	
+	static void fun(int x){
+		System.out.println("B1::test(int)");
+	}
 	public void test(int x,int y){
-		System.out.println("B1::test(int,int)");
+		System.out.println("B1::fun(int,int)");
+	}
+	static void fun(int x,int y){
+		System.out.println("B1::fun(int,int)");
 	}
 }
--- CDAC/Java_Team_6/B2.java
+++ CDAC/Java_Team_6/B2.java
@@ -7,6 +7,8 @@ class B2 extends B1{
 		B1 b1 = new B1();
 		b1.test(10);
 		b1.test(10,20);
+		B1.fun(5);
+		B1.fun(40,50);
 		//b1.test(10,20,30);
 	}
 }
