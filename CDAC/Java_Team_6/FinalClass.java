fd1439764f1b144fa13a849ad7effeee7eba4e27
nipun-madaan <nipun_madaan@yahoo.com>
2020-07-08 23:12:38 +0530

    Final Class

---
+++ CDAC/Java_Team_6/FinalClass.java
@@ -0,0 +1,13 @@
+final class Fun{
+	int x=10;
+	public static void main(String[] args){
+		Fun f = new Fun();
+		System.out.println(f.x);
+	}
+}
+/*class Gun extends Fun{    // final class can't be inherited
+	public static void main(String[] args){
+		Gun f = new Gun();
+		System.out.println(f.x);
+	}
+}*/
