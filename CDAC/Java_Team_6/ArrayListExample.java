8675796cf15b6af377e45e527c7d51aff87d5df7
nipun-madaan <nipun_madaan@yahoo.com>
2020-07-23 22:10:25 +0530

    Collection - ArrayList

---
+++ CDAC/Java_Team_6/ArrayListExample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class AL{
+	public static void main(String[] args){
+		ArrayList l = new ArrayList();
+		l.add(10);
+		l.add("Mehul");
+		l.add(52.5);
+		System.out.println(l);
+		System.out.println(l.size());
+		System.out.println(l.get(1));
+	}
+}
