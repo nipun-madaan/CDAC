8aea966a89e91e10127400dd54cf1e3e338dea8a
nipun-madaan <nipun_madaan@yahoo.com>
2020-09-21 16:33:51 +0530

    CharAt function example

---
+++ CDAC/KhargharJuhuJava/src/cdac/W.java
@@ -0,0 +1,10 @@
+package cdac;
+
+class W{
+	public static void main(String[] args){
+		String s = "Nidhi";
+		System.out.println(s);
+		for(int i = 0;i<s.length();i++)
+			System.out.println(s.charAt(i));
+	}
+}
