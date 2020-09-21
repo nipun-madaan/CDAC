3cfa7445d2a6b04720c4cf019fafcbf6280c26bd
nipun-madaan <nipun_madaan@yahoo.com>
2020-09-21 16:28:57 +0530

    basic example

--- CDAC/KhargharJuhuJava/src/cdac/G.java
+++ CDAC/KhargharJuhuJava/src/cdac/G.java
@@ -4,7 +4,7 @@ class G{
 	public static void main(String[] args){
 		int i = 10;
 		System.out.println(i==10);
-		while(true){
+		while(true){   //while(1) will give error as it requires the boolean not int
 			System.out.println("Hello");
 		}
 	}
