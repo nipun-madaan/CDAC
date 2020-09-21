6e446384e6b090dfa2211df53dd5b04feed034ef
nipun-madaan <nipun_madaan@yahoo.com>
2020-09-21 16:06:50 +0530

    print vs println and class vs object

---
+++ CDAC/KhargharJuhuJava/src/cdac/Hello.java
@@ -0,0 +1,13 @@
+package cdac;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!!");   //println - print and moves the cursor to the next line
+		System.out.print("Hello World!!!");      //print - print and cursor remains at the same line
+		System.out.print("Hello World!!!");
+		Hello h = new Hello();
+	}
+
+}
