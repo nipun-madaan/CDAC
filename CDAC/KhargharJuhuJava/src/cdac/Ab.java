3b7a7733882ef03bc938e3cb2cf30fb7eb5e0cf2
nipun-madaan <nipun_madaan@yahoo.com>
2020-09-21 16:09:39 +0530

    Scanner class

---
+++ CDAC/KhargharJuhuJava/src/cdac/Ab.java
@@ -0,0 +1,31 @@
+package cdac;
+import java.util.Scanner;
+
+class Ab{
+	static Scanner sc;
+	static void sum() {
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+	public static void main(String[] args){
+		sc = new Scanner(System.in);
+		int a = sc.nextInt();   //int
+		float b = sc.nextFloat();
+		double c = sc.nextDouble();
+		String d = sc.next(); //Nipun Madaan
+		String e = sc.nextLine();
+		System.out.println(a+" b="+b+" c="+c+" d="+d+" e="+e);
+		//System.out.println(a+b+c+d+e);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		sum();
+		sc.close();
+		
+	}
+}
+
+//radius and area and circumference
