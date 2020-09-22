21670942da115b171656f184ac6b693458971ef2
nipun-madaan <nipun_madaan@yahoo.com>
2020-09-22 11:58:26 +0530

    array example

---
+++ CDAC/KhargharJuhuJava/src/cdac/Array.java
@@ -0,0 +1,9 @@
+class Array{
+	public static void main(String[] args){
+		//int[] arr;  //error it should be initialized
+		int[] arr = new int[3];
+		for(int a:arr){
+			System.out.println(a);   //0 0 0
+		}
+	}
+}
