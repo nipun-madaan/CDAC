6e977d2f8577bea76e26d3c4321af81a67d3d65e
nipun-madaan <nipun_madaan@yahoo.com>
2020-12-09 06:09:10 +0530

    LinkedList-Advance

--- CDAC/KhargharJuhuJava/src/collection/LinkedListEx.java
+++ CDAC/KhargharJuhuJava/src/collection/LinkedListEx.java
@@ -1,5 +1,9 @@
 package collection;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class LinkedListEx {
 	
@@ -43,13 +47,52 @@ public class LinkedListEx {
  		l.removeLastOccurrence(10);     //remove the first recuring value from last 
 		System.out.println(l);
 		System.out.println(l.hashCode());   //gives the hash code of the linkedlist 
-		//removeif
-		//retainall
-		//set
-		//subList
-		//toArray
-		//descendingIterator
-		//iterator
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+		ll.add(5);
+		ll.add(10);
+		ll.add(15);
+		ll.add(20);
+		ll.add(25);
+		ll.add(30);
+		//ll.removeIf(n->n%2!=0);//removeif   //boolean even(n){
+		System.out.println(ll);				//	if(n%2==0) return true;else return false;
+		//l.removeAll(ll);									//	}
+		l.retainAll(ll);//retainall
+		System.out.println(l);
+		System.out.println(ll);
+		ll.set(1,50);//set
+		System.out.println(ll);
+		List<Integer> sl = ll.subList(2, 6);//subList
+		System.out.println(sl);
+		Object[] arr = ll.toArray();//toArray
+		for(Object o : arr) {
+			System.out.print(o+" ");
+		}
+		System.out.println();
+		Iterator<Integer> itr = ll.iterator(); //iterator
+		while(itr.hasNext()) {
+			System.out.print(itr.next()+" ");
+		}
+		System.out.println();
+		Iterator<Integer> dItr = ll.descendingIterator();//descendingIterator
+		while(dItr.hasNext()) {
+			System.out.print(dItr.next()+" ");
+		}
+		System.out.println();
+		ListIterator<Integer> lItr = ll.listIterator();
+		while(lItr.hasNext()) {
+			System.out.print(lItr.next()+" ");
+		}
+		System.out.println();
+		ListIterator<Integer> lItr1 = ll.listIterator(2);
+		while(lItr1.hasNext()) {
+			System.out.print(lItr1.next()+" ");
+		}
+		
+		/*LinkedList<Integer> l1 = new LinkedList<Integer>();
+		ArrayList<Integer> a1 = new ArrayList<Integer>();
+		List<Integer> l2 = new LinkedList<Integer>();
+		List<Integer> a2 = new ArrayList<Integer>();*/
 	}
 	
 }
