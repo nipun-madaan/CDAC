b50aaf4de09bfd46f10b272528606f96fc8c41d1
nipun-madaan <nipun_madaan@yahoo.com>
2020-12-10 07:54:53 +0530

    HashSet - Advanced

--- CDAC/KhargharJuhuJava/src/collection/HashSetEx.java
+++ CDAC/KhargharJuhuJava/src/collection/HashSetEx.java
@@ -3,6 +3,7 @@
  */
 package collection;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -16,24 +17,28 @@ public class HashSetEx {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		HashSet h = new HashSet();
+		HashSet<Integer> h = new HashSet<Integer>();
 		System.out.println(h.add(10));
-		h.add(10.3);
+		h.add(10);
 		h.add(1001);
 		h.add(1010);
-		System.out.println(h.add(10.3));
+		System.out.println(h.add(10));
 		System.out.println(h);
 		//default initial capacity - 16
 		//default load ratio - 75%
 		HashSet<Integer> h1 = new HashSet<Integer>(32);   //here 32 is the initial capacity
 		HashSet<Integer> h2 = new HashSet<Integer>(32, 0.90f); //here 32 is initial capacity
 									// and 0.90f is load ratio
-		System.out.println(h.size());
-		Iterator itr = h.iterator();
-		while(itr.hasNext()) {
+		System.out.println(h.size());  // gives size of the set
+		Iterator<Integer> itr = h.iterator();  //gives the object of iterator which is used to
+		while(itr.hasNext()) {         //iterate over the elements
 			System.out.print(itr.next()+" ");
 		}
-		
+		System.out.println();
+		h.forEach(n->{    //for each loop to iterate over all the elements and perform any action like square of the no
+			System.out.println(n*n);
+		});
+		//Collections.sort(h);  //sorting works  on the list
 	}
 
 }
