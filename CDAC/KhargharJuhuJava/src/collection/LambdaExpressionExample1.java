51f3a1549f44087d337e52005f37076dfe828c14
nipun-madaan <nipun_madaan@yahoo.com>
2020-12-25 19:13:58 +0530

    Lambda Expression parameterless

---
+++ CDAC/KhargharJuhuJava/src/collection/LambdaExpressionExample1.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package collection;
+
+import java.util.Scanner;
+
+/**
+ * @author Nipun Madaan
+ *
+ */
+interface Abc{
+	public void say(String name);
+}
+public class LambdaExpressionExample1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		Abc a =(name)->{
+			System.out.println("Hello "+name);
+		};
+		a.say(str);
+	}
+}
